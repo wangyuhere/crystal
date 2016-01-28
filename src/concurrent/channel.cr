84889d238abac6c7dfd7f89c55916ac36190863d
Juan Wajnerman <jwajnerman@manas.com.ar>
2016-01-28 19:23:12 -0300

    WIP

--- src/concurrent/channel.cr
+++ src/concurrent/channel.cr
@@ -23,7 +23,7 @@ abstract class Channel(T)
 
   def close
     @closed = true
-    Scheduler.enqueue @receivers
+    Scheduler.current.enqueue @receivers
     @receivers.clear
     nil
   end
@@ -95,7 +95,7 @@ abstract class Channel(T)
       end
 
       ops.each &.wait
-      Scheduler.reschedule
+      Scheduler.current.reschedule
       ops.each &.unwait
     end
   end
@@ -161,13 +161,13 @@ class Channel::Buffered(T) < Channel(T)
     while full?
       raise_if_closed
       @senders << Fiber.current
-      Scheduler.reschedule
+      Scheduler.current.reschedule
     end
 
     raise_if_closed
 
     @queue << value
-    Scheduler.enqueue @receivers
+    Scheduler.current.enqueue @receivers
     @receivers.clear
 
     self
@@ -177,11 +177,11 @@ class Channel::Buffered(T) < Channel(T)
     while empty?
       yield if @closed
       @receivers << Fiber.current
-      Scheduler.reschedule
+      Scheduler.current.reschedule
     end
 
     @queue.shift.tap do
-      Scheduler.enqueue @senders
+      Scheduler.current.enqueue @senders
       @senders.clear
     end
   end
@@ -208,7 +208,7 @@ class Channel::Unbuffered(T) < Channel(T)
     while @has_value
       raise_if_closed
       @senders << Fiber.current
-      Scheduler.reschedule
+      Scheduler.current.reschedule
     end
 
     raise_if_closed
@@ -220,7 +220,7 @@ class Channel::Unbuffered(T) < Channel(T)
     if receiver = @receivers.shift?
       receiver.resume
     else
-      Scheduler.reschedule
+      Scheduler.current.reschedule
     end
   end
 
@@ -231,7 +231,7 @@ class Channel::Unbuffered(T) < Channel(T)
       if sender = @senders.shift?
         sender.resume
       else
-        Scheduler.reschedule
+        Scheduler.current.reschedule
       end
     end
 
@@ -239,7 +239,7 @@ class Channel::Unbuffered(T) < Channel(T)
 
     @value.tap do
       @has_value = false
-      Scheduler.enqueue @sender.not_nil!
+      Scheduler.current.enqueue @sender.not_nil!
     end
   end
 
